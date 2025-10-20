94a47a619cadfb2ddb6159fbc7177ffcb0192a0a
darklorddeveloper <darklorddeveloper@gmail.com>
2025-10-20 17:48:58 +0900

    setup things

---
+++ Assets/_Project/DotsCollection/QuickAuthorization/SetupClassSystem.cs
@@ -0,0 +1,48 @@
+using Unity.Entities;
+using Unity.Collections;
+using UnityEngine;
+
+namespace DarkLordGame
+{
+    public partial class SetupClassSystem : SystemBase
+    {
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            RequireForUpdate<SetupClassComponent>();
+        }
+        protected override void OnUpdate()
+        {
+            var em = EntityManager;
+
+        // Grab entities that still need initialization (or remove the tag query if you want to run unconditionally)
+        var query = SystemAPI.QueryBuilder().WithAll<SetupClassComponent>().Build();
+        using var entities = query.ToEntityArray(Allocator.Temp);
+
+        foreach (var e in entities)
+        {
+            // Enumerate *all* component types on the entity
+            using var types = em.GetComponentTypes(e, Allocator.Temp);
+
+            foreach (var ct in types)
+            {
+                if (!ct.IsManagedComponent) continue;
+
+                // Get the managed System.Type of this component
+                var managedType = ct.GetManagedType();
+
+                // Only care about components deriving from InitComponent
+                if (!typeof(ClassComponentData).IsAssignableFrom(managedType)) continue;
+
+                // Fetch the actual managed component instance and call Init
+                var compObj = em.GetComponentObject<ClassComponentData>(e, managedType);
+                compObj.Init();
+            }
+
+            // Done for this entity
+            em.RemoveComponent<SetupClassComponent>(e);
+        }
+        }
+    }
+}
--- Assets/_Project/DotsCollection/QuickAuthorization/StructAuthorizer.cs
+++ Assets/_Project/DotsCollection/QuickAuthorization/StructAuthorizer.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 
 namespace DarkLordGame
 {
-    public class QuickAuthorizationStructData<T1> : MonoBehaviour where T1 : unmanaged, IComponentData
+    public class StructAuthorizer<T1> : MonoBehaviour where T1 : unmanaged, IComponentData
     {
         public TransformUsageFlags flags = TransformUsageFlags.None;
-        public bool isActiveFromStart1 = true; // todo: this
         public T1 data1;
-        
 
     }
 
-    public class QuickAuthorizinonStructBaker<T, T1> : Baker<T> where T : QuickAuthorizationStructData<T1>
+    public class StructBaker<T, T1> : Baker<T> where T : StructAuthorizer<T1>
     where T1 : unmanaged, IComponentData
     {
         public override void Bake(T authoring)
@@ -21,7 +19,7 @@ namespace DarkLordGame
             AddComponent(e, authoring.data1);
         }
     }
-    public class QuickAuthorizationStructData<T1, T2> : MonoBehaviour
+    public class StructAuthorizer<T1, T2> : MonoBehaviour
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     {
@@ -32,7 +30,7 @@ namespace DarkLordGame
     }
 
 
-    public class QuickAuthorizinonStructBaker<T, T1, T2> : Baker<T> where T : QuickAuthorizationStructData<T1, T2>
+    public class StructBaker<T, T1, T2> : Baker<T> where T : StructAuthorizer<T1, T2>
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     {
@@ -44,7 +42,7 @@ namespace DarkLordGame
         }
     }
 
-    public class QuickAuthorizationStructData<T1, T2, T3> : MonoBehaviour
+    public class StructAuthorizer<T1, T2, T3> : MonoBehaviour
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     where T3 : unmanaged, IComponentData
@@ -57,7 +55,7 @@ namespace DarkLordGame
     }
 
 
-    public class QuickAuthorizinonStructBaker<T, T1, T2, T3> : Baker<T> where T : QuickAuthorizationStructData<T1, T2, T3>
+    public class StructBaker<T, T1, T2, T3> : Baker<T> where T : StructAuthorizer<T1, T2, T3>
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     where T3 : unmanaged, IComponentData
@@ -71,7 +69,7 @@ namespace DarkLordGame
         }
     }
 
-    public class QuickAuthorizationStructData<T1, T2, T3, T4> : MonoBehaviour
+    public class StructAuthorizer<T1, T2, T3, T4> : MonoBehaviour
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     where T3 : unmanaged, IComponentData
@@ -86,7 +84,7 @@ namespace DarkLordGame
     }
 
 
-    public class QuickAuthorizinonStructBaker<T, T1, T2, T3, T4> : Baker<T> where T : QuickAuthorizationStructData<T1, T2, T3, T4>
+    public class StructBaker<T, T1, T2, T3, T4> : Baker<T> where T : StructAuthorizer<T1, T2, T3, T4>
     where T1 : unmanaged, IComponentData
     where T2 : unmanaged, IComponentData
     where T3 : unmanaged, IComponentData
