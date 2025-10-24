22fd414a10e65485d8f1b3fa860cd9441890a654
darklorddeveloper <darklorddeveloper@gmail.com>
2025-10-24 09:57:54 +0900

    fixed minor camera code change

--- Assets/_Project/Scripts/Camera/CameraMovementSystem.cs
+++ Assets/_Project/Scripts/Camera/CameraMovementSystem.cs
@@ -20,16 +20,24 @@ namespace DarkLordGame
             if (SystemAPI.ManagedAPI.TryGetSingleton<MainCamera>(out var cam) == false) return;
             if (cam.initialized == false) return;
 
-            if(SystemAPI.TryGetSingleton<CameraMovement>(out var camMovement) == false) return;
-            if(isChangingAngle)
+            if (SystemAPI.TryGetSingleton<CameraMovement>(out var camMovement) == false) return;
+            if (isChangingAngle)
             {
                 timeCount += SystemAPI.Time.DeltaTime;
-                cam.cameraRootTransform.position = Vector3.Slerp(startPos, camMovement.position, timeCount / camMovement.movePeriod);
-                cam.cameraRootTransform.rotation = Quaternion.Slerp(startRot, Quaternion.Euler(camMovement.eulerRotation), timeCount / camMovement.movePeriod);
-                isChangingAngle = timeCount >= camMovement.movePeriod;
+                isChangingAngle = timeCount < camMovement.movePeriod;
+                if (isChangingAngle == false)
+                {
+                    cam.cameraRootTransform.position = camMovement.position;
+                    cam.cameraRootTransform.rotation = Quaternion.Euler(camMovement.eulerRotation);
+                }
+                else
+                {
+                    cam.cameraRootTransform.position = Vector3.Slerp(startPos, camMovement.position, timeCount / camMovement.movePeriod);
+                    cam.cameraRootTransform.rotation = Quaternion.Slerp(startRot, Quaternion.Euler(camMovement.eulerRotation), timeCount / camMovement.movePeriod);
+                }
             }
             if (SystemAPI.TryGetSingleton<CurrentPhase>(out var currentPhase) == false) return;
-            if(currentPhase.isChangingPhase == false)
+            if (currentPhase.isChangingPhase == false)
             {
                 return;
             }
