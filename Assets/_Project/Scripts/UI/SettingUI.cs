17a05f39d0be47517b0ce1842e57d3f2630069bc
darklorddeveloper <darklorddeveloper@gmail.com>
2025-10-28 23:55:46 +0900

    adjusting for compile speed

---
+++ Assets/_Project/Scripts/UI/SettingUI.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+namespace DarkLordGame
+{
+    public class SettingUI : MonoBehaviour
+    {
+        public UI3DToggleGroup group;
+        public List<UI3DFlagToggle> languageFlag;
+    }
+}
