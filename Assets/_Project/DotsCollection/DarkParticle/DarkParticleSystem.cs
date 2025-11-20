2b08ecbb04783e2e95842c07bf67421a50cb24c0
darklorddeveloper <darklorddeveloper@gmail.com>
2025-11-20 13:14:03 +0900

    implemented scale

--- Assets/_Project/DotsCollection/DarkParticle/DarkParticleSystem.cs
+++ Assets/_Project/DotsCollection/DarkParticle/DarkParticleSystem.cs
@@ -1,7 +1,9 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace DarkLordGame
@@ -10,7 +12,7 @@ namespace DarkLordGame
     public partial struct ParticleLifeTimeUpdateJob : IJobEntity
     {
         public float deltaTime;
-        public EntityCommandBuffer.ParallelWriter ecb;
+        [NativeDisableParallelForRestriction] public EntityCommandBuffer.ParallelWriter ecb;
         public void Execute([ChunkIndexInQuery] int chunk, Entity e, ref Particle particleLifeTime)
         {
             if (particleLifeTime.isInfiniteLoop)
@@ -23,8 +25,8 @@ namespace DarkLordGame
                 {
                     particleLifeTime.loopCount--;
                     particleLifeTime.timeCount -= particleLifeTime.lifeTime;
-                    if(particleLifeTime.loopCount <= 0)
-                    ecb.SetComponentEnabled<Particle>(chunk, e, false);
+                    if (particleLifeTime.loopCount <= 0)
+                        ecb.SetComponentEnabled<Particle>(chunk, e, false);
                     return;
                 }
             }
@@ -32,6 +34,28 @@ namespace DarkLordGame
             particleLifeTime.currentRate = math.clamp(particleLifeTime.timeCount / particleLifeTime.lifeTime, 0, 1);
         }
     }
+
+    [BurstCompile]
+    public partial struct ParticleSetupStartSizeJob : IJobEntity
+    {
+        [NativeDisableParallelForRestriction] public EntityCommandBuffer.ParallelWriter ecb;
+        public void Execute([ChunkIndexInQuery] int chunk, Entity entity, in ParticleStartSize particleStartSize, ref ParticleSizeOverTime sizeOverTime, in LocalTransform localTransform)
+        {
+            sizeOverTime.maxSize = localTransform.Scale;
+            ecb.SetComponentEnabled<ParticleStartSize>(chunk, entity, false);
+        }
+    }
+
+    [BurstCompile]
+    public partial struct ParticleSetupSizeOverTimeJob : IJobEntity
+    {
+        public void Execute(in Particle particle, ref ParticleSizeOverTime sizeOverTime, ref LocalTransform localTransform)
+        {
+            ref var curve = ref sizeOverTime.data.Value;
+            localTransform.Scale = curve.Evaluate(particle.currentRate) * sizeOverTime.maxSize;
+        }
+    }
+
 
     [BurstCompile]
     public partial struct DarkParticleSystem : ISystem
@@ -44,15 +68,26 @@ namespace DarkLordGame
             float deltaTime = SystemAPI.Time.DeltaTime;
 
             var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.TempJob);
+            var ecbParallel = ecb.AsParallelWriter();
             var lifetimeJob = new ParticleLifeTimeUpdateJob
             {
                 deltaTime = deltaTime,
-                ecb = ecb.AsParallelWriter(),
+                ecb = ecbParallel,
             };
-            var handleLifetimeJob = lifetimeJob.ScheduleParallel(state.Dependency);
-            handleLifetimeJob.Complete();
+        
+            var handle = lifetimeJob.ScheduleParallel(state.Dependency);
+        
+            var setupScaleJob = new ParticleSetupStartSizeJob
+            {
+                ecb = ecbParallel
+            };
+            var handle2 = setupScaleJob.ScheduleParallel(handle);
+            handle2.Complete();
             ecb.Playback(state.EntityManager);
             ecb.Dispose();
+
+            var sizeJob = new ParticleSetupSizeOverTimeJob();
+            sizeJob.ScheduleParallel();
         }
     }
 }
